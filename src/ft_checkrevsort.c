aa7e8809b95aa3f6237ae5d41f8cdf1dba37ca5a
lvallie <lvallie@student.21-school.ru>
2021-06-05 17:09:34 +0300

    stupid shit

--- src/ft_checkrevsort.c
+++ src/ft_checkrevsort.c
@@ -1,13 +1,13 @@
 #include "../push_swap.h"
 
-int	ft_checksort(t_stack **stack)
+int	ft_checkrevsort(t_stack **stack)
 {
 	t_stack	*tmp;
 
 	tmp = *stack;
 	while (tmp->next)
 	{
-		if (tmp->number > tmp->next->number)
+		if (tmp->number < tmp->next->number)
 			return (0);
 		else
 			tmp = tmp->next;
